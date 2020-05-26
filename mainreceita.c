ba5d7cc1b4073845a975a3dcd7f59a599ca85b58
aichatassa <57256843+aichatassa@users.noreply.github.com>
2020-05-26 10:39:55 -0300

    Update mainreceita.c

--- mainreceita.c
+++ mainreceita.c
@@ -2,3 +2,50 @@
 #include <stdlib.h>
 #include "receita.h"
 
+int main() {
+	
+	char menu;
+	
+	printf("LIVRO DE RECEITAS\n");
+	printf("\nOpcoes:\n");
+	printf("< > - Navegar\n");
+	printf("* - Marcar como usada\n");
+	printf("a - Alterar\n");
+	printf("! - Apagar receita\n");
+	printf("+ - Inserir receita\n");
+	printf("s - Sair do livro de receitas\n");
+	scanf("%c", &menu);
+	getchar();
+	
+	do{
+	
+		switch(menu) {
+			case '<':
+				break;
+				
+			case '>':
+				break;
+				
+			case '*':
+				break;
+				
+			case 'a':
+				break;
+				
+			case '!':
+				break;
+				
+			case '+':
+				break;
+				
+			case 's':
+				break;
+				
+			default:
+				printf("Opcao invalida!\n");
+				break;
+		}
+	while(menu != 's');
+	
+	return 0;
+}
