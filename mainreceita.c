5dd8feb316ac6bd86d9f30a93a67fd808f1a60d8
carolalua <57256788+carolalua@users.noreply.github.com>
2020-05-28 11:56:32 -0300

    Update mainreceita.c

--- mainreceita.c
+++ mainreceita.c
@@ -4,26 +4,29 @@
 
 int main() {
 	
+	TReceita* atual = NULL;
+	TReceita* primeiro = NULL;
 	char menu;
 	
-	printf("LIVRO DE RECEITAS\n");
-	printf("\nOpcoes:\n");
-	printf("< > - Navegar\n");
-	printf("* - Marcar como usada\n");
-	printf("a - Alterar\n");
-	printf("! - Apagar receita\n");
-	printf("+ - Inserir receita\n");
-	printf("s - Sair do livro de receitas\n");
-	scanf("%c", &menu);
-	getchar();
-	
 	do{
+		printf("LIVRO DE RECEITAS\n");
+		printf("\nOpcoes:\n");
+		printf("< > - Navegar\n");
+		printf("* - Marcar como usada\n");
+		printf("a - Alterar\n");
+		printf("! - Apagar receita\n");
+		printf("+ - Inserir receita\n");
+		printf("s - Sair do livro de receitas\n");
+		scanf("%c", &menu);
+		getchar();
 	
 		switch(menu) {
 			case '<':
+	        	    	atual = navegar_receitas(atual, menu);
 				break;
 				
 			case '>':
+				atual = navegar_receitas(atual, menu);
 				break;
 				
 			case '*':
@@ -36,6 +39,8 @@ int main() {
 				break;
 				
 			case '+':
+				inserir_receitas(&primeiro);
+				atual = primeiro;
 				break;
 				
 			case 's':
